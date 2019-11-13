ab8d3d8cd7a410e184870bd2120cff26ce83ffac
Jakub Filipiak <jakub.filipiak@billennium.com>
2019-11-13 15:06:15 +0100

    Initial commit

---
+++ src/main/java/pl/filipiak/jakub/vehicleRental/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.filipiak.jakub.vehicleRental;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VehicleRentalApplication.class);
+	}
+
+}
---
+++ src/test/java/pl/filipiak/jakub/vehicleRental/VehicleRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filipiak.jakub.vehicleRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
